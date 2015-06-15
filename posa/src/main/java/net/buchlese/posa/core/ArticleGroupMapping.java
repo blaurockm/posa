92fe8c20c0ced1820eee6161e5486d99edb43ee6
Markus Blaurock <blaurock.markus@googlemail.com>
2015-06-15 18:12:02 +0200

    fix warengruppen-mapping again

--- posa/src/main/java/net/buchlese/posa/core/ArticleGroupMapping.java
+++ posa/src/main/java/net/buchlese/posa/core/ArticleGroupMapping.java
@@ -1,5 +1,8 @@
 package net.buchlese.posa.core;
 
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
 import net.buchlese.posa.api.bofc.ArticleGroup;
 import net.buchlese.posa.api.pos.KassenVorgang;
 
@@ -10,6 +13,29 @@ public class ArticleGroupMapping {
 			if (grp.getWargrindex().contains(vorg.getWarGrIndex().toString())) {
 				return grp;
 			}
+		}
+		for (ArticleGroup grp : ArticleGroup.getArticleGroups().values()) {
+			if (grp.getKey().equals(ArticleGroup.NONE.getKey()) || grp.getMatch() == null) {
+				// mit nix wollen wir nicht vergleichen
+				continue;
+			}
+			Predicate<String> p = Pattern.compile(grp.getMatch()).asPredicate();
+			if (vorg.getArtikelNummer() != null && p.test(vorg.getArtikelNummer())) {
+				return grp;
+			}
+			if (vorg.getIsbn() != null && p.test(vorg.getIsbn())) {
+				return grp;
+			}
+			if (vorg.getMatchCode() != null && p.test(vorg.getMatchCode())) {
+				return grp;
+			}
+			if (grp.getMatch().startsWith("mwst=")) {
+				Character mwst = grp.getMatch().charAt(5);
+				if (mwst.equals(vorg.getMWSt())) {
+					return grp;
+				}
+			}
+			
 		}
 		return ArticleGroup.NONE;
 	}
--- posa/src/main/java/net/buchlese/posa/jdbi/pos/KassenVorgangDAO.java
+++ posa/src/main/java/net/buchlese/posa/jdbi/pos/KassenVorgangDAO.java
@@ -15,22 +15,22 @@ import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
 @RegisterMapper(KassenVorgangMapper.class)
 public interface KassenVorgangDAO {
 
-	@SqlQuery("select [dbo].posakassvorg.*, [dbo].artikel.wargrindex from [dbo].posakassvorg join [dbo].artikel on [dbo].posakassvorg.artikelident = [dbo].artikel.artikelident where belegnr = :belegnr and kassennr = :kassennr ")
+	@SqlQuery("select [dbo].posakassvorg.*, [dbo].artikel.wargrindex from [dbo].posakassvorg left join [dbo].artikel on [dbo].posakassvorg.artikelident = [dbo].artikel.artikelident where belegnr = :belegnr and kassennr = :kassennr ")
 	List<KassenVorgang> fetchForBeleg(@BindBean KassenBeleg beleg);
 
-	@SqlQuery("select [dbo].posakassvorg.*, [dbo].artikel.wargrindex from [dbo].posakassvorg join [dbo].artikel on [dbo].posakassvorg.artikelident = [dbo].artikel.artikelident  where datum> :datum order by datum asc ")
+	@SqlQuery("select [dbo].posakassvorg.*, [dbo].artikel.wargrindex from [dbo].posakassvorg left join [dbo].artikel on [dbo].posakassvorg.artikelident = [dbo].artikel.artikelident  where datum> :datum order by datum asc ")
 	List<KassenVorgang> fetchAllAfter(@Bind("datum") DateTime maxDatum);
 
-	@SqlQuery("select [dbo].posakassvorg.*, [dbo].artikel.wargrindex from [dbo].posakassvorg join [dbo].artikel on [dbo].posakassvorg.artikelident = [dbo].artikel.artikelident  where datum between :vonDatum  and :bisDatum ")
+	@SqlQuery("select [dbo].posakassvorg.*, [dbo].artikel.wargrindex from [dbo].posakassvorg left join [dbo].artikel on [dbo].posakassvorg.artikelident = [dbo].artikel.artikelident  where datum between :vonDatum  and :bisDatum ")
 	List<KassenVorgang> fetchAllBetween(@Bind("vonDatum") DateTime fromDate, @Bind("bisDatum") DateTime tillDate);
 
 	@SqlQuery("select zahlungsbetrag from [dbo].kassenbelege where belegnr = :belegnr")
 	BigDecimal fetchZahlbetrag(@Bind("belegnr") int belegNr);
 
-	@SqlQuery("select [dbo].posakassvorg.*, [dbo].artikel.wargrindex from [dbo].posakassvorg join [dbo].artikel on [dbo].posakassvorg.artikelident = [dbo].artikel.artikelident  where belegnr = :belegnr and kassennr = :kassennr ")
+	@SqlQuery("select [dbo].posakassvorg.*, [dbo].artikel.wargrindex from [dbo].posakassvorg left join [dbo].artikel on [dbo].posakassvorg.artikelident = [dbo].artikel.artikelident  where belegnr = :belegnr and kassennr = :kassennr ")
 	List<KassenVorgang> fetchForBeleg(@Bind("belegnr")int belegNr, @Bind("kassennr") int kassenNummer);
 
-	@SqlQuery("select [dbo].posakassvorg.*, [dbo].artikel.wargrindex from [dbo].posakassvorg join [dbo].artikel on [dbo].posakassvorg.artikelident = [dbo].artikel.artikelident  where belegnr = :belegnr and lfdNummer = :belegidx ")
+	@SqlQuery("select [dbo].posakassvorg.*, [dbo].artikel.wargrindex from [dbo].posakassvorg left join [dbo].artikel on [dbo].posakassvorg.artikelident = [dbo].artikel.artikelident  where belegnr = :belegnr and lfdNummer = :belegidx ")
 	KassenVorgang fetch(@Bind("belegnr") long belegNr, @Bind("belegidx") int belegIdx);
 	
 }
