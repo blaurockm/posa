3e664566d8df6f91f87dd4fe572a77ca5f32241e
Markus Blaurock <blaurock@dig.de>
2016-08-05 18:51:02 +0200

    WIP

--- verw/src/main/java/net/buchlese/bofc/api/coupon/Coupon.java
+++ verw/src/main/java/net/buchlese/bofc/api/coupon/Coupon.java
@@ -2,12 +2,12 @@ package net.buchlese.bofc.api.coupon;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Transient;
-import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -24,7 +24,7 @@ public class Coupon {
 	private int pointid;
 
 	@JsonProperty
-	@Transient
+	@Column(name="customer_id", insertable=false, updatable=false)
 	private int customerId;  // die Schule
 	@JsonProperty
 	private String pupilsname;
@@ -43,6 +43,7 @@ public class Coupon {
 
 	@JsonIgnore
 	@ManyToOne
+	@JoinColumn(name="customer_id")
 	private Subscriber customer; // die Schule
 	public Subscriber getCustomer() {
 		return customer;
